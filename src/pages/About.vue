7456b66ac3752cb34daaff083ff23f6cd3693c2b
Mark Yuan <mark@webasone.com>
2025-12-10 02:02:42 +0000

    a

---
+++ src/pages/About.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { asoneDataHTML } from "@/shared/utils";
+
+const AboutPage = ref("");
+
+if (asoneDataHTML.About) {
+  AboutPage.value = asoneDataHTML.About.tcode;
+}
+</script>
+
+<template>
+  <div v-if="AboutPage" v-html="AboutPage"></div>
+</template>
